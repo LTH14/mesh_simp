2161e303a2bfafcbac7e4ba996fa7908d0c3fc6d
litianhong <litianhong@litianhongdeMacBook-Air.local>
2016-11-26 08:53:15 +0800

    Initial Commit

---
+++ mesh_simp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mesh_simp
+//
+//  Created by litianhong on 2016/11/26.
+//  Copyright © 2016年 litianhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
